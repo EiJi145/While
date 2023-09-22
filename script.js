9d9d455b6bca52c91d57d48ba4a006a1c776d798
Ary Thevenin <145431222+EiJi145@users.noreply.github.com>
2023-09-22 11:51:30 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+"use strict"
+
+//          WHILE
+/**
+ * Declarer une varaible a qui est egale a 3
+ * tant que a est plus petit que 9 
+ * incremente a de 1
+ * si a est egal a 8 on arrete la boucle 
+ * si a est egal a 7 on revient au debut de la boucle 
+ * on console log(a)
+ */
+let a=3;
+while(a<9){
+    temps++
+    if(a==7){
+        continue
+    }
+    if(a==8){
+        break
+    }
+    console.log(a);
+}
+console.log("J'ai oblitérer la boucle au bout de "+aT+" tours");
